8a46472be0f2208b3e74d259efb2f21d551dc47f
Nick Alymaras <nikos.alymaras@onelity.com>
2021-11-18 12:29:06 +0200

    update function

--- netlify/functions/getProgramme/getProgramme.js
+++ netlify/functions/getProgramme/getProgramme.js
@@ -1,17 +1,23 @@
-// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
-const handler = async (event) => {
-  try {
-    const subject = event.queryStringParameters.name || 'World'
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
-    }
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() }
-  }
-}
+var cron = require("node-cron");
 
-module.exports = { handler }
+var m = 0;
+cron.schedule("* * * * *", () => {
+	m++;
+});
+
+const handler = async (event) => {
+	try {
+		const subject = event.queryStringParameters.name || "World";
+		return {
+			statusCode: 200,
+			body: JSON.stringify({ message: `Hello ${m}` }),
+			// // more keys you can return:
+			// headers: { "headerName": "headerValue", ... },
+			// isBase64Encoded: true,
+		};
+	} catch (error) {
+		return { statusCode: 500, body: error.toString() };
+	}
+};
+
+module.exports = { handler };
